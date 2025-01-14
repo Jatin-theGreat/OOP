320d3644025f4138fd489b19a9aae0b8fa5e121f
Jatin-theGreat <jatindayama123@gmail.com>
2025-01-14 10:51:55 +0530

    Update Largest_of_3.cpp

--- Largest_of_3.cpp
+++ Largest_of_3.cpp
@@ -5,10 +5,16 @@ int main()
 	int n1,n2,n3;
 	cout<<"Enter three numbers : ";
 	cin>>n1>>n2>>n3;
-	if(n1>n2 && n1>n3)
+	if(n1==n2 && n1==n3)
+	cout<<"The largest number is : "<<n1;
+	else if(n1==n2 && n1>n3)
+	cout<<"The largest number is : "<<n1;
+	else if(n2==n3 && n2>n1)
+	cout<<"The largest number is : "<<n2;
+	else if(n1>n2 && n1>n3)
 	cout<<"The largest number is : "<<n1;
 	else if(n2>n1 && n2>n3)
 	cout<<"The largest number is : "<<n2;
-	else
+	else if(n3>n1 &&n3>n2)
 	cout<<"The largest number is : "<<n3;
 }
